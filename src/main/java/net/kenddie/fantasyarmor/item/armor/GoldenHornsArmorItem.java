32203566c174f39534970fbc69a7da8ce32352c6
mon1tor <westxlebyshek@gmail.com>
2024-08-10 14:57:16 +0200

    Golder horns armor renderer fix

--- src/main/java/net/kenddie/fantasyarmor/item/armor/GoldenHornsArmorItem.java
+++ src/main/java/net/kenddie/fantasyarmor/item/armor/GoldenHornsArmorItem.java
@@ -1,6 +1,7 @@
 package net.kenddie.fantasyarmor.item.armor;
 
 import net.kenddie.fantasyarmor.entity.client.armor.model.lib.FAArmorModel;
+import net.kenddie.fantasyarmor.entity.client.armor.render.lib.FAArmorRenderer;
 import net.kenddie.fantasyarmor.item.armor.lib.FAArmorItem;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
@@ -11,7 +12,7 @@ public class GoldenHornsArmorItem extends FAArmorItem {
 
     @Override
     protected GeoArmorRenderer<? extends FAArmorItem> createArmorRenderer() {
-        return new GeoArmorRenderer<>(new FAArmorModel<>(
+        return new FAArmorRenderer<>(new FAArmorModel<>(
                 "geo/golden_horns_armor.geo.json",
                 "textures/armor/golden_horns_armor.png"
         ));
