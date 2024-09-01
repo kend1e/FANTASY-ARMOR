552eadb210f110b041f069f5305d1f84d9f14c20
kend1e <kotbmope@gmail.com>
2024-09-01 19:44:52 +0200

    Modificators fix

--- src/main/java/net/kenddie/fantasyarmor/item/armor/lib/FAArmorItem.java
+++ src/main/java/net/kenddie/fantasyarmor/item/armor/lib/FAArmorItem.java
@@ -2,9 +2,7 @@ package net.kenddie.fantasyarmor.item.armor.lib;
 
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
-import net.minecraft.ChatFormatting;
 import net.minecraft.client.model.HumanoidModel;
-import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.ai.attributes.Attribute;
@@ -13,67 +11,54 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.TooltipFlag;
-import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
 import software.bernie.geckolib.core.animation.AnimatableManager;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
-import java.util.List;
 import java.util.UUID;
 import java.util.function.Consumer;
 
 public abstract class FAArmorItem extends ArmorItem implements GeoItem {
-    protected final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
-
-    private static final UUID KNOCKBACK_RESISTANCE_MODIFIER_UUID = UUID.randomUUID();
-    private static final UUID MOVEMENT_SPEED_MODIFIER_UUID = UUID.randomUUID();
-    private static final UUID MAX_HEALTH_MODIFIER_UUID = UUID.randomUUID();
-    private static final UUID ATTACK_DAMAGE_MODIFIER_UUID = UUID.randomUUID();
-    private static final UUID ATTACK_SPEED_MODIFIER_UUID = UUID.randomUUID();
-    private static final UUID LUCK_MODIFIER_UUID = UUID.randomUUID();
-
-
+    private final AnimatableInstanceCache cache = new SingletonAnimatableInstanceCache(this);
 
     private final Multimap<Attribute, AttributeModifier> attributeModifiers;
 
-    public FAArmorItem(Type type, double knockbackResistance, double movementSpeed, double maxHealth, double attackDamage, double attackSpeed, double luck) {
+    protected FAArmorItem(Type type, double knockbackResistance, double movementSpeed, double maxHealth, double attackDamage, double attackSpeed, double luck) {
         super(ArmorMaterials.NETHERITE, type, new Properties().stacksTo(1));
 
         ImmutableMultimap.Builder<Attribute, AttributeModifier> builder = ImmutableMultimap.builder();
 
         if (knockbackResistance > 0) {
-            builder.put(Attributes.KNOCKBACK_RESISTANCE, new AttributeModifier(KNOCKBACK_RESISTANCE_MODIFIER_UUID, "Armor knockback resistance", knockbackResistance, AttributeModifier.Operation.ADDITION));
+            builder.put(Attributes.KNOCKBACK_RESISTANCE, new AttributeModifier(UUID.randomUUID(), "Armor knockback resistance", knockbackResistance, AttributeModifier.Operation.ADDITION));
         }
 
         if (movementSpeed > 0) {
-            builder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(MOVEMENT_SPEED_MODIFIER_UUID, "Armor movement speed", movementSpeed, AttributeModifier.Operation.MULTIPLY_TOTAL));
+            builder.put(Attributes.MOVEMENT_SPEED, new AttributeModifier(UUID.randomUUID(), "Armor movement speed", movementSpeed, AttributeModifier.Operation.MULTIPLY_TOTAL));
         }
 
         if (maxHealth > 0) {
-            builder.put(Attributes.MAX_HEALTH, new AttributeModifier(MAX_HEALTH_MODIFIER_UUID, "Armor health gain", maxHealth, AttributeModifier.Operation.ADDITION));
+            builder.put(Attributes.MAX_HEALTH, new AttributeModifier(UUID.randomUUID(), "Armor health gain", maxHealth, AttributeModifier.Operation.ADDITION));
         }
 
         if (attackDamage > 0) {
-            builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(ATTACK_DAMAGE_MODIFIER_UUID, "Armor attack damage", attackDamage, AttributeModifier.Operation.MULTIPLY_TOTAL));
+            builder.put(Attributes.ATTACK_DAMAGE, new AttributeModifier(UUID.randomUUID(), "Armor attack damage", attackDamage, AttributeModifier.Operation.MULTIPLY_TOTAL));
         }
 
         if (attackSpeed > 0) {
-            builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(ATTACK_SPEED_MODIFIER_UUID, "Armor attack speed", attackSpeed, AttributeModifier.Operation.MULTIPLY_TOTAL));
+            builder.put(Attributes.ATTACK_SPEED, new AttributeModifier(UUID.randomUUID(), "Armor attack speed", attackSpeed, AttributeModifier.Operation.MULTIPLY_TOTAL));
         }
 
         if (luck > 0) {
-            builder.put(Attributes.LUCK, new AttributeModifier(LUCK_MODIFIER_UUID, "Armor luck", luck, AttributeModifier.Operation.ADDITION));
+            builder.put(Attributes.LUCK, new AttributeModifier(UUID.randomUUID(), "Armor luck", luck, AttributeModifier.Operation.ADDITION));
         }
 
-        this.attributeModifiers = builder.build();
+        attributeModifiers = builder.build();
     }
 
     @Override
@@ -83,7 +68,7 @@ public abstract class FAArmorItem extends ArmorItem implements GeoItem {
 
             @Override
             public @NotNull HumanoidModel<?> getHumanoidArmorModel(LivingEntity livingEntity, ItemStack itemStack, EquipmentSlot equipmentSlot, HumanoidModel<?> original) {
-                if(renderer == null) {
+                if (renderer == null) {
                     renderer = createArmorRenderer();
                 }
 
