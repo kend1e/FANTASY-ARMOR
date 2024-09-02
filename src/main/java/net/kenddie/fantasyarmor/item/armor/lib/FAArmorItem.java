58ac30ed6ade86dafb401967a9f1491701edbea0
kend1e <kotbmope@gmail.com>
2024-09-02 15:25:03 +0200

    Armor tooltips

--- src/main/java/net/kenddie/fantasyarmor/item/armor/lib/FAArmorItem.java
+++ src/main/java/net/kenddie/fantasyarmor/item/armor/lib/FAArmorItem.java
@@ -3,6 +3,7 @@ package net.kenddie.fantasyarmor.item.armor.lib;
 import com.google.common.collect.ImmutableMultimap;
 import com.google.common.collect.Multimap;
 import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
@@ -12,10 +13,13 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterials;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.extensions.common.IClientItemExtensions;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 import software.bernie.geckolib.animatable.GeoItem;
 import software.bernie.geckolib.core.animatable.instance.AnimatableInstanceCache;
 import software.bernie.geckolib.core.animatable.instance.SingletonAnimatableInstanceCache;
@@ -60,6 +64,14 @@ public abstract class FAArmorItem extends ArmorItem implements GeoItem {
         }
 
         attributeModifiers = builder.build();
+    }
+
+    @Override
+    public void appendHoverText(ItemStack stack, @Nullable Level world, List<Component> tooltip, TooltipFlag flag) {
+        super.appendHoverText(stack, world, tooltip, flag);
+
+        String translationKey = this.getDescriptionId() + ".tooltip";
+        tooltip.add(Component.translatable(translationKey));
     }
 
     @Override
