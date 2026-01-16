2c4e9fe7108863d688e72e42456827e8ee375b80
Kenddie <kotbmope@gmail.com>
2026-01-16 19:56:35 +0100

    Fabric 1.21.10 project base

---
+++ appFabric/1.21.10/src/main/java/net/kenddie/fantasyarmor/client/render/FAClientEventHandler.java
@@ -0,0 +1,10 @@
+package net.kenddie.fantasyarmor.client.render;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FAClientEventHandler implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ appFabric/1.21.10/src/main/java/net/kenddie/fantasyarmor/mixin/FARenderMixin.java
@@ -0,0 +1,9 @@
+package net.kenddie.fantasyarmor.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftServer.class)
+public class FARenderMixin {
+
+}
